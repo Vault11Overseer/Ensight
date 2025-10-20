6921bfe24a4bc512d3036c336a3f850e38ff5e8a
Vault11Overseer <cherokeedreemer@gmail.com>
2025-10-20 13:10:02 -0400

    get this live

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/ensight/", // important for subfolder deployment
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // relative paths
+  build: {
+    target: "es2015", // produce older JS that works in Apache without module support
+  },
+});
