21fd9d9bc93ebdeb15271a9e5ea124b17ca3389b
Vault11Overseer <cherokeedreemer@gmail.com>
2025-11-04 12:19:23 -0500

    work on backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,20 +2,30 @@
 // import react from "@vitejs/plugin-react";
 
 // export default defineConfig(({ mode }) => ({
-//   base: mode === "production" ? "https://bcimedia.com/ensight/" : "/",
+//   base: mode === "production" ? "/ensight/" : "/",
 //   plugins: [react()],
 //   optimizeDeps: {
 //     exclude: ["fsevents"],
 //   },
 // }));
 
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/ensight/" : "/",
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ["fsevents"],
+  build: {
+    outDir: "dist", // default, make sure this is present
+    emptyOutDir: true, // cleans previous builds
+    rollupOptions: {
+      output: {
+        // ensures chunk filenames go into assets/
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
   },
 }));
